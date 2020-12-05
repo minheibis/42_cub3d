d7203d3bc82d150a008471d5f303513d0c0bedc7
yuuki634 <yuukihenmi16@gmail.com>
2020-12-05 15:24:44 +0900

    norm 13

--- srcs/free_xpm.c
+++ srcs/free_xpm.c
@@ -6,28 +6,28 @@
 /*   By: hyuki <hyuki@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 12:09:52 by hyuki             #+#    #+#             */
-/*   Updated: 2020/12/05 14:16:55 by hyuki            ###   ########.fr       */
+/*   Updated: 2020/12/05 15:23:43 by hyuki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int		free_xpm_N(t_s *s, int rv)
+int		free_xpm_n(t_s *s, int rv)
 {
 	mlx_destroy_image(s->g.mlx, s->g.ws.n.ptr);
 	return (rv);
 }
 
-int		free_xpm_S(t_s *s, int rv)
+int		free_xpm_s(t_s *s, int rv)
 {
-	free_xpm_N(s, rv);
+	free_xpm_n(s, rv);
 	mlx_destroy_image(s->g.mlx, s->g.ws.s.ptr);
 	return (rv);
 }
 
 int		free_xpm_w(t_s *s, int rv)
 {
-	free_xpm_S(s, rv);
+	free_xpm_s(s, rv);
 	mlx_destroy_image(s->g.mlx, s->g.ws.w.ptr);
 	return (rv);
 }

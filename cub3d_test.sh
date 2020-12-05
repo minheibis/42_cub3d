787e293257ad7ed05e3de04b9a411b8d1572c869
yuuki634 <yuukihenmi16@gmail.com>
2020-12-05 17:09:45 +0900

    norm 26

---
+++ cub3d_test.sh
@@ -0,0 +1,92 @@
+echo "./cub3D ./maps/test_1_normal_map.cub --save"
+./cub3D ./maps/test_1_normal_map.cub --save
+mv cub3d.bmp test_1_normal_map.bmp
+
+echo "./cub3D ./maps/test_2_large_resol.cub --save"
+./cub3D ./maps/test_2_large_resol.cub --save
+mv cub3d.bmp test_2_large_resol.bmp
+
+echo "./cub3D ./maps/test_3_minus_resol_ERROR.cub --save"
+./cub3D ./maps/test_3_minus_resol_ERROR.cub --save
+
+echo "./cub3D ./maps/test_4_zero_resol_ERROR.cub --save"
+./cub3D ./maps/test_4_zero_resol_ERROR.cub --save
+
+echo "./cub3D ./maps/test_5_kaigyou_in_rows.cub --save"
+./cub3D ./maps/test_5_kaigyou_in_rows.cub --save
+mv cub3d.bmp test_5_kaigyou_in_rows.bmp
+
+echo "./cub3D ./maps/test_6_space_in_map_ERROR.cub --save"
+./cub3D ./maps/test_6_space_in_map_ERROR.cub --save
+
+echo "./cub3D ./maps/test_7_dual_R_ERROR.cub --save"
+./cub3D ./maps/test_7_dual_R_ERROR.cub --save
+
+echo "./cub3D ./maps/test_8_no_R_ERROR.cub --save"
+./cub3D ./maps/test_8_no_R_ERROR.cub --save
+
+echo "./cub3D ./maps/test_9_dual_WE_ERROR.cub --save"
+./cub3D ./maps/test_9_dual_WE_ERROR.cub --save
+
+echo "./cub3D ./maps/test_10_no_WE_ERROR.cub --save"
+./cub3D ./maps/test_10_no_WE_ERROR.cub --save
+
+echo "./cub3D ./maps/test_11_invalid_WE_ERROR.cub --save"
+./cub3D ./maps/test_11_invalid_WE_ERROR.cub --save
+
+echo "./cub3D ./maps/test_12_invalid_resol_ERROR.cub --save"
+./cub3D ./maps/test_12_invalid_resol_ERROR.cub --save
+
+echo "./cub3D ./maps/test_13_invalid_F_ERROR.cub --save"
+./cub3D ./maps/test_13_invalid_F_ERROR.cub --save
+
+echo "./cub3D ./maps/test_14_lack_F_ERROR.cub --save"
+./cub3D ./maps/test_14_lack_F_ERROR.cub --save
+
+echo "./cub3D ./maps/test_15_long_F_ERROR.cub --save"
+./cub3D ./maps/test_15_long_F_ERROR.cub --save
+
+echo "./cub3D ./maps/test_16_no_sprite_ERROR.cub --save"
+./cub3D ./maps/test_16_no_sprite_ERROR.cub --save
+
+echo "./cub3D ./maps/test_17_nomap_ERROR.cub --save"
+./cub3D ./maps/test_17_nomap_ERROR.cub --save
+
+echo "./cub3D ./maps/test_18_onelinemap_ERROR.cub --save"
+./cub3D ./maps/test_18_onelinemap_ERROR.cub --save
+
+echo "./cub3D ./maps/test_19_minimap.cub --save"
+./cub3D ./maps/test_19_minimap.cub --save
+mv cub3d.bmp test_19_minimap.bmp
+
+echo "./cub3D ./maps/test_20_openmap_ERROR.cub --save"
+./cub3D ./maps/test_20_openmap_ERROR.cub --save
+
+echo "./cub3D ./maps/test_21_spaceinmap.cub --save"
+./cub3D ./maps/test_21_spaceinmap.cub --save
+mv cub3d.bmp test_21_spaceinmap.bmp
+
+echo "./cub3D ./maps/test_22_invalid_num_ERROR.cub --save"
+./cub3D ./maps/test_22_invalid_num_ERROR.cub --save
+
+echo "./cub3D ./maps/test_23_two_players_ERROR.cub --save"
+./cub3D ./maps/test_23_two_players_ERROR.cub --save
+
+echo "./cub3D ./maps/test_24_three_players_ERROR.cub --save"
+./cub3D ./maps/test_24_three_players_ERROR.cub --save
+
+echo "./cub3D ./maps/test_25_large_map.cub --save"
+./cub3D ./maps/test_25_large_map.cub --save
+mv cub3d.bmp test_25_large_map.bmp
+
+echo "./cub3D ./maps/test_26_letters_aftermap_ERROR.cub --save"
+./cub3D ./maps/test_26_letters_aftermap_ERROR.cub --save
+
+echo "./cub3D ./maps/test_27_element_aftermap_ERROR.cub --save"
+./cub3D ./maps/test_27_element_aftermap_ERROR.cub --save
+
+echo "./cub3D ./maps/test_28_map_shape_plus.cub --save"
+./cub3D ./maps/test_28_map_shape_plus.cub --save
+
+#echo "./cub3D ./maps/test_2_large_resol.cub"
+#./cub3D ./maps/test_2_large_resol.cub
